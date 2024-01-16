fd3c5258ad153ef71b561bf0e81c15dc86ad0315
carpenterzoe <carpenterzoe@outlook.com>
2024-01-16 21:16:29 +0800

    feat: create Event entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -12,7 +12,9 @@ export class Coffee {
   @Column()
   brand: string;
 
-  // @Column('json', { nullable: true })
+  @Column({ default: 0 })
+  recommendations: number;
+
   @JoinTable()
   @ManyToMany(
     type => Flavor,
