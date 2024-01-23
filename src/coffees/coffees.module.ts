86719e042cac8a38cb5197c7c82fd369a9ece16a
carpenterzoe <carpenterzoe@outlook.com>
2024-01-23 08:27:03 +0800

    feat: 自定义providers -useValue

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,11 +6,21 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+class MockCoffeesService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
+
   // 在 CoffeesModule 中，向 Nest loC(控制反转) 容器注册 providers.
-  providers: [CoffeesService],
+  // providers: [CoffeesService],
+
+  providers: [
+    {
+      provide: CoffeesService,  // token 
+      useValue: new MockCoffeesService()  // value
+    }
+  ],
 
   /**
    * If we want to use the CoffeesService in another module,
