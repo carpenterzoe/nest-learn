d72f20250648e1a2e2ff1740e15754a66656d3ec
carpenterzoe <carpenterzoe@outlook.com>
2024-02-01 09:02:50 +0800

    feat: Protect Routes with Guards

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from './common/filter/http-exception/http-exception.filter'
+import { ApiKeyGuard } from './common/guards/api-key/api-key.guard'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
@@ -19,6 +20,7 @@ async function bootstrap() {
     })
   )
   app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalGuards(new ApiKeyGuard())
   await app.listen(3000)
 }
 bootstrap()
