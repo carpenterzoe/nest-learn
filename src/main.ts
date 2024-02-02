3ad52f948fa7057f917e205b25859ee8de151bd4
carpenterzoe <carpenterzoe@outlook.com>
2024-02-02 08:50:17 +0800

    feat: Add Pointcuts with Interceptors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from './common/filter/http-exception/http-exception.filter'
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor'
 // import { ApiKeyGuard } from './common/guards/api-key/api-key.guard'
 
 async function bootstrap() {
@@ -21,6 +22,7 @@ async function bootstrap() {
   )
   app.useGlobalFilters(new HttpExceptionFilter())
   // app.useGlobalGuards(new ApiKeyGuard()) // 在Common Module中注入了，因为 ApiKeyGuard 内部有其他依赖
+  app.useGlobalInterceptors(new WrapResponseInterceptor())
   app.enableCors()
   await app.listen(3000)
 }
