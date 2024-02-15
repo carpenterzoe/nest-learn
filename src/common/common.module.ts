14a760310ac1b12315c24e11428c96c039ca04a2
carpenterzoe <carpenterzoe@outlook.com>
2024-02-15 17:09:06 +0800

    feat: LoggingMiddleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { ApiKeyGuard } from './guards/api-key/api-key.guard'
 import { ConfigModule } from '@nestjs/config'
+import { LoggingMiddleware } from './middleware/logging/logging.middleware'
 
 // a module class where we can register any global enhancers we might take in the future.
 @Module({
@@ -13,4 +19,12 @@ import { ConfigModule } from '@nestjs/config'
     },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggingMiddleware).forRoutes('*')
+    consumer.apply(LoggingMiddleware).forRoutes({
+      path: 'coffees/*',
+      method: RequestMethod.GET,
+    })
+  }
+}
