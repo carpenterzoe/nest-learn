550adbacb8c9a885b5df405d3247aecc0d4587a0
carpenterzoe <carpenterzoe@outlook.com>
2024-02-16 10:34:15 +0800

    feat: Swagger Module

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from './common/filter/http-exception/http-exception.filter'
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor'
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 // import { ApiKeyGuard } from './common/guards/api-key/api-key.guard'
 
 async function bootstrap() {
@@ -28,6 +29,15 @@ async function bootstrap() {
     new TimeoutInterceptor()
   )
   app.enableCors()
+
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
